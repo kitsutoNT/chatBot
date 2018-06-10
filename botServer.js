237674d41da94189109501fb3384fa8853dd14c0
高橋信史 <takahashinobufumi@takahashishinjinoMacBook-puro.local>
2018-06-10 15:01:02 +0900

    Finished Documentation

--- botServer.js
+++ botServer.js
@@ -168,4 +168,4 @@ router
 // Use defined routers above.
 app.use(router.routes()).use(router.allowedMethods())
 // Listen to server.
-app.listen(3000)
+app.listen('https://simplechatbot0901.herokuapp.com/')
